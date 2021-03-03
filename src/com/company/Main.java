282d6f1ed40640ef6a67a52f25cf98ebf3e1a223
Luana Franca <21898@student.dorset-college.ie>
2021-03-03 15:30:32 +0000

    First Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,21 +11,33 @@ public class Main {
         final String DB_USER = "LuanaHF";
         final String DB_PASSWORD = "12232321";
 
-        try  {
+        try {
             Class.forName("com.mysql.jdbc.Driver");
 
             Connection con = DriverManager.getConnection(
                     DB_URL, DB_USER, DB_PASSWORD);
 
             Statement stmt = con.createStatement();
-            String sqlQuery = "select * from titanic";
+
+            String insertSQL = "insert into superheroes (id, firstName, lastName, heroName)\n" +
+                    "values (2, 'Peter', 'Parker', 'Spiderman')";
+
+            String insertNewSQL = "insert into superheroes (id, firstName, lastName, heroName)\n" +
+                    "values (3, 'Clark', 'Kent', 'Superman')";
+
+            stmt.executeUpdate(insertNewSQL);
+
+
+
+
+            String sqlQuery = "select * from superheroes";
 
             ResultSet rs = stmt.executeQuery(sqlQuery);
 
 
             ResultSetMetaData metaData = rs.getMetaData();
             int numberOfColumns = metaData.getColumnCount();
-            for(int i = 1; i<= numberOfColumns; i++) {
+            for (int i = 1; i <= numberOfColumns; i++) {
                 System.out.printf("%-8s\t", metaData.getColumnName(i));
             }
             System.out.println();
@@ -33,18 +45,16 @@ public class Main {
 
             while (rs.next()) {
                 // System.out.println(rs.getInt(1) + "\t  " + rs.getString(2) + " \t " + rs.getString(3));
-                System.out.println(rs.getInt(1) + "\t  " + rs.getInt(2) + " \t " + rs.getInt(3) + " \t " + rs.getString(4));
+                System.out.println(rs.getString(4));
             }
 
 
-
-
-        } catch (SQLException  e) {
+        } catch (SQLException e) {
             System.out.println(e.toString());
 
-        } catch(Exception e) {
+        } catch (Exception e) {
 
-        } finally{
+        } finally {
             // System.out.println("finally");
         }
 
